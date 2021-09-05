ac46622b9609291a38ee420356757f6d2129f3ac
Hayden Young <hi@hbjy.dev>
2021-09-05 16:39:30 +0100

    chore: include components in index.ts for bundler

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export * as Button from './components/Button';
+export * as Hero from './components/Hero';
+export * as Navbar from './components/Navbar';
+export * as Styler from './components/Styler';
